4a9c67356621c1d282e70ee40df606e5ca986c8f
seriouslucy <seriouslucy@outlook.com>
2025-02-06 22:47:08 -0600

    modified
    new images and css styling to solo project!

---
+++ data/index.js
@@ -0,0 +1,11 @@
+console.log('connected');
+
+const quotes = [];
+
+const newQuoteBtn = document.querySelector('.quotebtn')
+
+newQuoteBtn.addEventListener("click", clickHandler);
+
+function clickHandler() {
+    console.log('hello')
+}
